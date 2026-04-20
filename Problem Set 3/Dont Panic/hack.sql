2a95aba5a5f1fe1df37695f88310bc2e5a6abe03
Gabriel Manata <gabriel.m.p0804@gmail.com>
2026-04-20 10:48:29 -0300

    ...

--- Problem Set 3/Dont Panic/hack.sql
+++ Problem Set 3/Dont Panic/hack.sql
@@ -1,9 +1,14 @@
-UPDATE users 
-SET password = 'oops!'
+UPDATE users
+SET password = '982c0381c279d139fd221fce974916e7'
 WHERE username = 'admin';
 
 
 
 DELETE FROM user_logs WHERE type = 'update' AND new_username = 'admin';
 
-// Falta o utimo
+
+
+INSERT INTO user_logs (type, old_username, new_username, new_password)
+SELECT 'update', 'admin', 'admin', new_password
+FROM user_logs
+WHERE new_username = 'emily33';
